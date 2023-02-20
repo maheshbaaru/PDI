8bae068f0e7da3054775bc30693cf9e769681188
maheshbaaru <123924054+maheshbaaru@users.noreply.github.com>
2023-02-20 17:22:35 +0530

    h

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { PdiModule } from './modules/pdi/pdi.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PdiModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
